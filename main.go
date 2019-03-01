06653fef5a6f8bc43ee7f933d29defd379baffdc
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-03-01 08:57:03 +0100

    WIP and deploy test

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+func randomNo() string {
+	nopes := make([]string, 0)
+	nopes = append(nopes,
+		"How 'bout no?",
+		"NO",
+		"Nope",
+		"Nada",
+		"Njet!",
+		"Yesn't",
+		"Nein, Nein, Nein!")
+	return nopes[rand.Intn(len(nopes))]
+}
+
 func main() {
+	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -22,7 +39,9 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"noPhrase": randomNo(),
+		})
 	})
 
 	router.GET("/txt", func(c *gin.Context) {
